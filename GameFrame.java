8e475964db233d89de63d95a656782fab2d7e0dd
Funtikmq <veaceslav.bobeica.02@gmail.com>
2023-11-29 19:31:18 +0200

    Added Design Changes; Added Sounds;

--- GameFrame.java
+++ GameFrame.java
@@ -1,15 +1,21 @@
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
 
 public class GameFrame extends JFrame {
 
-    GameFrame(){
+
+    GameFrame() throws UnsupportedAudioFileException, LineUnavailableException, IOException {
         ImageIcon icon = new ImageIcon("images\\logo.png");
 
         this.setIconImage(icon.getImage());
-        this.add(new GamePanel());
         this.setTitle("Snake");
+        this.setBackground(new Color(157, 114, 31));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
+        this.add(new GamePanel());
         this.pack();
         this.setVisible(true);
         this.setLocationRelativeTo(null);
--- GamePanel.java
+++ GamePanel.java
@@ -1,17 +1,35 @@
+import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.ImageObserver;
 import java.awt.image.ImageProducer;
+import java.io.File;
+import java.io.IOException;
+import java.sql.Time;
 import java.util.Random;
+import java.util.TimerTask;
 
 
 public class GamePanel extends JPanel implements ActionListener{
 
+//         Images and Sound     \\
 
     Image apple = new ImageIcon("images\\apple.png").getImage();
     Image background = new ImageIcon("images\\background.jpg").getImage();
+    ImageIcon sound = new ImageIcon("images\\sound.png");
+    ImageIcon soundoff = new ImageIcon("images\\soundoff.png");
 
+    File eatSound = new File("audio\\eat.wav");
+    File gameMusic = new File("audio\\music.wav");
+    AudioInputStream eat = AudioSystem.getAudioInputStream(eatSound);
+    AudioInputStream mus = AudioSystem.getAudioInputStream(gameMusic);
+    Clip eating = AudioSystem.getClip();
+    Clip music = AudioSystem.getClip();
+    JButton soundButton;
+
+
+//          Images and Sound         \\
 
     static final int SCREEN_WIDTH = 600;
     static final int SCREEN_HEIGHT = 600;
@@ -20,29 +38,52 @@ public class GamePanel extends JPanel implements ActionListener{
     static final int DELAY=75;
     final int x[] = new int[GAME_UNITS];
     final int y[] = new int[GAME_UNITS];
-    int bodyPart=6;
-    int applesEaten=0;
-    int appleX;
-    int appleY;
-    char direction = 'R';
-    boolean running =false;
+    private int bodyPart=6;
+    private int applesEaten=0;
+    private int appleX;
+    private int appleY;
+    private char direction = 'R';
+    private boolean running =false;
     Timer timer;
     Random random;
-    GamePanel(){
+    private int arcStart=270;
+    private int arcX;
+    private int arcY;
+    GamePanel() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+
+        soundButton = new JButton();
+        soundButton.setBounds(SCREEN_WIDTH-35,SCREEN_HEIGHT+10,25,25);
+        soundButton.setFocusable(false);
+        soundButton.setBackground(new Color(0,0,0, 0));
+        soundButton.addActionListener(this);
+        soundButton.setIcon(sound);
+        soundButton.setBorder(null);
+
         random = new Random();
-        this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
-        this.setBackground(new Color(213, 185, 131));
+
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT+50));
+        this.setBackground(new Color(32, 128, 12));
         this.setFocusable(true);
         this.addKeyListener(new MyKeyAdapter());
+        this.add(soundButton);
+        this.setLayout(null);
 
+
+
+
+        music.open(mus);
+        eating.open(eat);
+        music.start();
         startGame();
     }
+
 
     public void startGame(){
         newApple();
         running=true;
         timer = new Timer(DELAY,this);
         timer.start();
+
     }
     public void paintComponent(Graphics g){
         super.paintComponent(g);
@@ -66,16 +107,16 @@ public class GamePanel extends JPanel implements ActionListener{
 
             for(int i=0;i<bodyPart;i++){
                 if(i==0){
-                    g.setColor(new Color(255,255,255));
-                    g.fillRect(x[i],y[i],UNIT_SIZE-1,UNIT_SIZE-1);
+                    g.setColor(new Color(0, 225, 169));
+                    g.fillArc(x[i]+arcX,y[i]+arcY,UNIT_SIZE,UNIT_SIZE,arcStart,180);
                 }
-                else {
-                    g.setColor(new Color(0, 0, 0));
-                    g.fillRect(x[i],y[i],UNIT_SIZE-1,UNIT_SIZE-1);
+                else{
+                    g.fillRect(x[i],y[i],UNIT_SIZE,UNIT_SIZE);
                 }
             }
-            g.setColor(Color.white);
-            g.drawString("Score: "+applesEaten,(SCREEN_WIDTH-metrics.stringWidth("Score: "+applesEaten))/2,g.getFont().getSize());
+            g.setColor(Color.WHITE);
+            g.drawImage(apple,20,SCREEN_HEIGHT+10,null);
+            g.drawString(":"+String.valueOf(applesEaten),40,SCREEN_HEIGHT+35);
         }
         else {
             gameOver(g);
@@ -102,33 +143,63 @@ public class GamePanel extends JPanel implements ActionListener{
     public void move(){
         for(int i=bodyPart;i>0;i--){
             x[i] = x[i-1];
-            y[i] = y[i-1];
+            y[i]= y[i-1];
         }
         switch (direction){
             case 'U':y[0] = y[0] - UNIT_SIZE;
+                arcStart=0;
+                arcX =0;
+                arcY=(UNIT_SIZE/2+1);
             break;
             case 'B':y[0] = y[0] + UNIT_SIZE;
+                arcStart=180;
+                arcX =0;
+                arcY=(UNIT_SIZE/2+1)*-1;
             break;
             case 'L':x[0] = x[0]-UNIT_SIZE;
+                arcStart=90;
+                arcX =(UNIT_SIZE/2+1);
+                arcY=0;
             break;
             case 'R':x[0] = x[0]+UNIT_SIZE;
+                arcStart=270;
+                arcX =(UNIT_SIZE/2+1)*-1;
+                arcY=0;
+            break;
         }
         switch (direction){
             case 'W':y[0] = y[0] - UNIT_SIZE;
+                arcStart=0;
+                arcX =0;
+                arcY=(UNIT_SIZE/2+1);
                 break;
             case 'S':y[0] = y[0] + UNIT_SIZE;
+                arcStart=180;
+                arcX =0;
+                arcY=(UNIT_SIZE/2+1)*-1;
                 break;
             case 'A':x[0] = x[0]-UNIT_SIZE;
+                arcStart=90;
+                arcX =(UNIT_SIZE/2+1);
+                arcY=0;
                 break;
             case 'D':x[0] = x[0]+UNIT_SIZE;
+                arcStart=270;
+                arcX =(UNIT_SIZE/2+1)*-1;
+                arcY=0;
+                break;
         }
 
+
     }
-    public void checkApple(){
+    public void checkApple() {
         if((x[0]==appleX)&&(y[0]==appleY)){
             bodyPart++;
             applesEaten++;
             newApple();
+
+            eating.setMicrosecondPosition(0);
+            eating.start();
         }
     }
     public void checkCollisions(){
@@ -149,6 +220,7 @@ public class GamePanel extends JPanel implements ActionListener{
         //Check if Head Touches Bottom Border
         if(y[0]>=SCREEN_HEIGHT){
             running=false;
+
         }//Check if Head Touches Top Border
         if(y[0]<0){
             running=false;
@@ -165,7 +237,6 @@ public class GamePanel extends JPanel implements ActionListener{
         g.setFont(new Font("Consolas",Font.BOLD,75));
         FontMetrics metrics1 = getFontMetrics(g.getFont());
         g.drawString("Game Over",(SCREEN_WIDTH-metrics1.stringWidth("Game Over"))/2,SCREEN_HEIGHT/4);
-        g.setColor(Color.white);
         g.setFont(new Font("Consolas",Font.BOLD,35));
         FontMetrics metrics2 = getFontMetrics(g.getFont());
         g.drawString("Score: "+applesEaten,(SCREEN_WIDTH-metrics2.stringWidth("Score: "+applesEaten))/2,SCREEN_HEIGHT/3);
@@ -188,10 +259,19 @@ public class GamePanel extends JPanel implements ActionListener{
         repaint();
     }
 
-
-
     @Override
     public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==soundButton){
+            if(music.isActive()){
+                music.stop();
+                soundButton.setIcon(soundoff);
+            }
+            else {
+                music.start();
+                soundButton.setIcon(sound);
+            }
+        }
+
         if(running){
             move();
             checkApple();
@@ -202,7 +282,7 @@ public class GamePanel extends JPanel implements ActionListener{
 
 
     public class MyKeyAdapter extends KeyAdapter {
-        @Override
+
         public void keyPressed(KeyEvent e){
             switch (e.getKeyCode()){
                 case KeyEvent.VK_LEFT:
@@ -254,8 +334,12 @@ public class GamePanel extends JPanel implements ActionListener{
                         if (direction!='A'){
                             direction='D';
                         }
+                    break;
                 }
             }
+
         }
+
+    public int getScore(){return applesEaten;}
     }
 
